d8d791a266da7ee4db0c7e7a550c27814513fee8
Macklem Curtis <macklem.curtis@gmail.com>
2022-12-14 21:37:27 -0500

    title

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Macklem Curtis';
+export const SITE_TITLE = 'Lemmack | Macklem Curtis';
 export const SITE_DESCRIPTION = 'Personal site of Macklem Curtis.';
